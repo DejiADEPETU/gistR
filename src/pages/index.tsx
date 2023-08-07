696f41c7c365a1e43019851307e96033184c98a7
Deji <dev.adepetu@gmail.com>
2023-08-07 17:21:58 -0400

    Added Post via prisma studio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
+import { SignIn, SignInButton, SignOutButton, useUser, SignUpButton } from "@clerk/nextjs";
+import {type NextPage} from "next";
+//import x from "x";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
 
-import { SignIn, SignInButton, SignOutButton, useUser, SignUpButton } from "@clerk/nextjs";
-
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
 
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -18,20 +19,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-
-
-
-
-
-        {!user.isSignedIn && <SignInButton/>}
-        {!!user.isSignedIn && <SignOutButton/>}
-      
-
-
-
-
-          
+        <div>
+          {!user.isSignedIn && <SignInButton/>}
+          {!!user.isSignedIn && <SignOutButton/>}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
-}
+};
